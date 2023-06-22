2276156549d14b17e4e5046f3874f0c22b93e27b
Евгений Кравец <129658755+Evgeniy761@users.noreply.github.com>
2023-06-22 21:10:52 +0300

    Файлы сайта-визитки

---
+++ js.js
@@ -0,0 +1,9 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+var button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+
+var button = document.querySelector('button');
+button.addEventListener('click', function() {
+	window.location.href = "https://prikolists.club/wp-content/uploads/2019/06/23-434.jpg";
+	  });
